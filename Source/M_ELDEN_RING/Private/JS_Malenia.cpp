e699417957b0e296476d086161d5985cc32cbaea
jeongsoll <80992713+jeongsoll@users.noreply.github.com>
2024-12-05 17:31:51 +0900

    기본 세팅

---
+++ Source/M_ELDEN_RING/Private/JS_Malenia.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JS_Malenia.h"
+
+// Sets default values
+AJS_Malenia::AJS_Malenia()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AJS_Malenia::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AJS_Malenia::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AJS_Malenia::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
