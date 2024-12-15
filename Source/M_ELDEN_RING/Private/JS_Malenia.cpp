e0e3a3c6f11298758a694bf64b3efb6bd2604232
jeongsoll <80992713+jeongsoll@users.noreply.github.com>
2024-12-15 22:02:48 +0900

    저장용

--- Source/M_ELDEN_RING/Private/JS_Malenia.cpp
+++ Source/M_ELDEN_RING/Private/JS_Malenia.cpp
@@ -3,10 +3,12 @@
 
 #include "JS_Malenia.h"
 
+#include "Components/CapsuleComponent.h"
+
 // Sets default values
 AJS_Malenia::AJS_Malenia()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
